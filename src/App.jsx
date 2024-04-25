fb351f724254b6c8b57b1f871379cc3dd4f3fcd4
Aviral000 <avimalik02@gmail.com>
2024-04-25 11:26:32 +0530

    final change

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import "./App.css"
+import "./App.css";
+import Countries from './components/Countries';
 
 export default function App() {
   return (
     <div>
-      Hello
+      <Countries />
     </div>
   )
 }
---
+++ src/components/Countries.jsx
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+
+export default function countries() {
+const [countries, setCountries] = useState([]);
+
+const apiCall = async() => {
+    const response = await axios.get("https://restcountries.com/v3.1/all");
+    setCountries(response.data);
+}
+
+useEffect(() => {
+    apiCall();
+}, []);
+
+  return (
+    <div>
+      <div className='card' style={{ display: "flex", flexWrap: "wrap", gap: "3rem" }}>
+        {countries.map((country) => {
+            return <div className='card-size' key={country.cca2}>
+                <div className='middle-card'>
+                    <div className='inner-card'>
+                        <img src={country.flags.png} alt={country.flags.alt} />
+                        <h2>{country.name.official}</h2>
+                    </div>
+                </div>
+            </div>
+        })}
+      </div>
+    </div>
+  )
+}
